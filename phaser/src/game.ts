e2ae571e9f0a13889b5865ad3f0f6fab0c2fa8c6
Param Singh <me@param.codes>
2023-06-26 22:34:49 +0530

    limit movement to land tiles

--- phaser/src/game.ts
+++ phaser/src/game.ts
@@ -3,6 +3,8 @@ import Phaser from "phaser";
 class MainScene extends Phaser.Scene {
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
   private character!: Phaser.Physics.Arcade.Sprite;
+  private mapLayer!: Phaser.Tilemaps.TilemapLayer;
+
   constructor() {
     super({ key: "main" });
   }
@@ -18,9 +20,19 @@ class MainScene extends Phaser.Scene {
   create() {
     const map = this.make.tilemap({ key: "map" });
     const tileset = map.addTilesetImage("tilemap-separated-test", "tiles");
-    map.createLayer("Tile Layer 1", tileset!, 0, 0);
-    this.character = this.physics.add.sprite(100, 100, "characters", 0);
+    this.mapLayer = map.createLayer("Tile Layer 1", tileset!, 0, 0)!;
+    this.character = this.physics.add.sprite(350, 100, "characters", 0);
     this.cursors = this.input.keyboard!.createCursorKeys();
+  }
+
+  private isOnLandTile(tileX: number, tileY: number): boolean {
+    const tile = this.mapLayer.getTileAt(tileX, tileY);
+    if (tile) {
+      // Assuming "normal land" tiles have an index of 1.
+      // Change this depending on your map data.
+      return tile.index === 43;
+    }
+    return false;
   }
 
   update() {
@@ -28,15 +40,30 @@ class MainScene extends Phaser.Scene {
 
     this.character.setVelocity(0, 0);
 
-    if (this.cursors.up?.isDown) {
+    const nextTileX = Math.floor(this.character.x / 16); // assuming each tile is 16 pixels
+    const nextTileY = Math.floor(this.character.y / 16); // adjust this if your tiles are a different size
+
+    if (
+      this.cursors.up?.isDown &&
+      this.isOnLandTile(nextTileX, nextTileY - 1)
+    ) {
       this.character.setVelocityY(-speed);
-    } else if (this.cursors.down?.isDown) {
+    } else if (
+      this.cursors.down?.isDown &&
+      this.isOnLandTile(nextTileX, nextTileY + 1)
+    ) {
       this.character.setVelocityY(speed);
     }
 
-    if (this.cursors.left?.isDown) {
+    if (
+      this.cursors.left?.isDown &&
+      this.isOnLandTile(nextTileX - 1, nextTileY)
+    ) {
       this.character.setVelocityX(-speed);
-    } else if (this.cursors.right?.isDown) {
+    } else if (
+      this.cursors.right?.isDown &&
+      this.isOnLandTile(nextTileX + 1, nextTileY)
+    ) {
       this.character.setVelocityX(speed);
     }
 
